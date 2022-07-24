192511e22a7c6099dee18de204c367e6924d09f3
Marion Emeric Bourgogne <marion@360medics.com>
2022-07-24 03:21:29 +0200

    add methods to aggregate data per 15 min

--- src/Service/StandardizePowerhouseService.php
+++ src/Service/StandardizePowerhouseService.php
@@ -4,14 +4,88 @@ namespace App\Service;
 
 class StandardizePowerhouseService
 {
-    public function aggregateDataByDate(array $data, array $powerhouses, int $fromDate, int $toDate)
+    /**
+     * @throws \JsonException
+     */
+    public function aggregateDataByDate(array $data, array $powerhouses, int $fromDate, int $toDate): array
     {
-        // TODO - step 1: Pour chaque Centrale vérifier qu'il y ait une ligne par tranche de 15 min sinon => prendre la valeur précédente et insérer une ligne correspondant à cette tranche horaire
-        // => Pour Barnsley prendre la valeur et la diviser par 2 ($powerhouses[1]['step']/15)
-        // => Pour Hounslow prendre la valeur et la diviser par 4 ($powerhouses[2]['step']/15)
+        // Harmonize field names
+        $data = $this->replaceArrayKeys($data);
 
-        // TODO - step 2: Normaliser en parallèle les clés des tableaux ("start", "end", "power")
+        // For each Central, check that there is one line per 15 min. otherwise => take the previous value and insert a line corresponding to this time slot
+        $dataByStepOf15 = $this->dividePowerByStepOf15($data, $powerhouses, $fromDate);
 
-        // TODO - step 3: Additionner le "power" de chaque ligne de chaque tableau des centrales
+        // Add the "power" of each line of each powerhouse per 15 min
+        return $this->mergeData($dataByStepOf15);
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    private function replaceArrayKeys(array $array): array
+    {
+        $arrayWithFinalKeys = [];
+        foreach ($array as $key => $secondaryArray) {
+            $json = str_replace(array('debut', 'start_time', 'fin', 'end_time', 'value', 'valeur'), array('start', 'start', 'end', 'end', 'power', 'power'), json_encode($secondaryArray, JSON_THROW_ON_ERROR));
+            $arrayWithFinalKeys[$key] = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
+        }
+        return $arrayWithFinalKeys;
+    }
+
+    /**
+     * @param array $data
+     * @param array $powerhouses
+     * @param int $fromDate
+     * @return array
+     */
+    private function dividePowerByStepOf15(array $data, array $powerhouses, int $fromDate): array
+    {
+        $result = [];
+        foreach ($data as $key => $powerhouseArray) {
+            $startDate = $fromDate;
+            $indexOfKey = array_search($key, array_column($powerhouses, 'name'), true);
+            $divider = $powerhouses[$indexOfKey]['step'] / 15;
+
+            $i = 0;
+            foreach ($powerhouseArray as $item) {
+                $endDate = strtotime('+15 minutes', $startDate);
+                while ($item->start > $startDate) {
+                    $result[$key][] = [
+                        'start' => $startDate,
+                        'end' => $endDate,
+                        'power' => $result[$key][$i - 1]['power'],
+                    ];
+                    $startDate = strtotime('+15 minutes', $startDate);
+                    $endDate = strtotime('+15 minutes', $startDate);
+                    $i++;
+                }
+
+                $result[$key][] = [
+                    'start' => $startDate,
+                    'end' => $endDate,
+                    'power' => $item->power / $divider,
+                ];
+                $startDate = strtotime('+15 minutes', $startDate);
+                $i++;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     **/
+    private function mergeData(array $data): array
+    {
+        $result = [];
+        foreach ($data as $powerhouseArray) {
+            foreach ($powerhouseArray as $key => $item) {
+                $item['power'] = isset($result[$key]) ? $result[$key]['power'] + $item['power'] : $item['power'];
+                $result[$key] = $item;
+            }
+        }
+
+        return $result;
     }
 }
